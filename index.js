01d92deb98726331d14cc363f6794693d60e24a5
GirardRampen18 <116690944+GirardRampen18@users.noreply.github.com>
2023-05-03 18:04:53 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// const http        = require("http");
+const express = require('express');
+const app     = express();
+const port    = 3000;
+const dataMember  = require("./members.js");
+const dataUsers   = require("./users.js");
+const moment      = require("moment");
+
+app.get("/", (req, res)=> res.send("This is the Home Page"));
+
+app.get("/about", (req, res)=> 
+            res.json({
+                    status: "success",
+                    message: "response success",
+                    description: "Exercise #03",
+                    date:moment().format(),
+                    data: dataMember,
+            }
+            ));
+
+app.get("/users", (req, res)=>
+            res.json(dataUsers)
+)
+
+app.listen(port, ()=>
+    console.log(`Server running at http://localhost:${port}`)
+);
+// const server = http.createServer((req, res) =>{
+//     const url = req.url;
+//     res.statusCode = 200;
+
+//     if (url === "/"){
+//         res.setHeader('Content-Type', 'text/plain');
+//         res.write("This Is the Home Page");
+//     }
+//     else if (url === "/about"){
+//         res.setHeader('Content-Type', 'text/plain');
+//         res.write(
+//             JSON.stringify({
+//                 status: "success",
+//                 message: "response success",
+//                 description: "Exercise #03",
+//                 date:moment().format(),
+//                 data: dataMember,
+//             })
+//         )
+//     }
+//     else if (url === "/users"){
+//         res.setHeader("conten-type", "text/json");
+//         res.write(JSON.stringify(dataUsers));
+//     }
+//     res.end();
+// })
+
+// const hostname = '127.0.0.1'; // atau localhost
+// const port = 3000;
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
